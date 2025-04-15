481fd9be96dfb4381ba6f4f69e25d53daca9eb3f
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2025-04-15 13:14:39 +0530

    First Commit

---
+++ hosting_setup/setHosting.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import fs from "fs";
+
+import { configDotenv } from "dotenv";
+
+configDotenv({
+  path: "./.env",
+});
+console.log(process.env.VITE_BACKEND_URL);
+try {
+  const data = await axios.get(
+    `${process.env.VITE_BACKEND_URL}/hosting/get-site-static-data`
+  );
+  //changed into recent update
+  const siteData = data.data.siteStaticData[0];
+  console.log( JSON.stringify(siteData))
+
+  fs.writeFileSync("./staticData.json", JSON.stringify(siteData));
+  console.log("Data written to staticData.json");
+} catch (error) {
+  console.log(error);
+}
